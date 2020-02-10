fe471e6c028d9cdb1247417ebcfec96d4069c7f4
CzechHek <42714212+CzechHek@users.noreply.github.com>
2020-02-10 22:54:34 +0100

    Now supports 1.4

--- Scripts/BasicWinner.js
+++ Scripts/BasicWinner.js
@@ -4,13 +4,13 @@ mindelay = value.createInteger("MinDelay", 2400, 0, 3500);
 module = {
     name: "BasicWinner",
     values: [maxdelay, mindelay],
-    version: 2.0,
+    version: 2.1,
     author: "CzechHek",
     onPacket: function (event) {
-        if (event.getPacket() instanceof Java.type('net.minecraft.network.play.server.S02PacketChat') && event.getPacket().getChatComponent().getUnformattedText().match("Opis slovo")) {
+        if (event instanceof S02PacketChat && event.getChatComponent().getUnformattedText().match("Opis slovo")) {
             delay(~~(Math.random() * (maxdelay.get() - mindelay.get() + 1)) + mindelay.get(), function () {mc.thePlayer.sendChatMessage(event.getPacket().getChatComponent().getUnformattedText().split(" ")[4])});
         }
     },
 }
 
-script.import("Core.lib"); script.import("Timing.lib");
+script.import("Core.lib"); script.import("Timing.lib"); script.import("Packets.lib");
