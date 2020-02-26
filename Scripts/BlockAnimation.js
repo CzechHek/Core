6e372737e76b6cc056219d0ae9ccfc68b78d8e66
CzechHek <42714212+CzechHek@users.noreply.github.com>
2020-02-26 23:27:43 +0100

    Added support for manual blocking

--- Scripts/BlockAnimation.js
+++ Scripts/BlockAnimation.js
@@ -1,16 +1,16 @@
 list = [
     animation = value.createInteger("Animation", 3, 0, 7),
     vibration = value.createFloat("Vibration", 0, 0, 5),
-    fake = value.createBoolean("Fake", false),
+    fake = value.createBoolean("Fake", false)
 ]
 
 module = {
     name: "BlockAnimation",
     author: "CzechHek",
-    version: 1.3,
+    version: 1.4,
     values: list,
     onMotion: function () {
-        mc.thePlayer.itemInUseCount = fake.get() && !KillAura.getValue("AutoBlock").get() && KillAuraClass.target && !!(heldItem = mc.thePlayer.getHeldItem()) && heldItem.getItem() instanceof Java.type("net.minecraft.item.ItemSword") ? 1337 : mc.thePlayer.itemInUseCount >= 1337 ? 0 : mc.thePlayer.itemInUseCount;
+        mc.thePlayer.itemInUseCount = (fake.get() && !KillAura.getValue("AutoBlock").get() && KillAuraClass.target && !!(heldItem = mc.thePlayer.getHeldItem()) && heldItem.getItem() instanceof Java.type("net.minecraft.item.ItemSword")) || mc.thePlayer.isBlocking() ? 1337 : mc.thePlayer.itemInUseCount >= 1337 ? 0 : mc.thePlayer.itemInUseCount;
         (KillAuraClass.blockingStatus || mc.thePlayer.itemInUseCount >= 1337) && (mc.thePlayer.swingProgress = [0.52, 0.58, 0.64, 0.7, 0.76, 0.82, 0.88, 0.94][animation.get()], mc.thePlayer.prevSwingProgress = mc.thePlayer.swingProgress - (0.01 * vibration.get()));
     },
     onDisable: function () {
