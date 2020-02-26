08583319f055af22e646d551d862e521d929b09d
CzechHek <42714212+CzechHek@users.noreply.github.com>
2020-02-26 19:14:52 +0100

    Added Vibration, Fake AutoBlock

---
+++ BlockAnimation.js
@@ -0,0 +1,27 @@
+list = [
+    animation = value.createInteger("Animation", 3, 0, 7),
+    vibration = value.createFloat("Vibration", 0, 0, 5),
+    fake = value.createBoolean("Fake", false),
+]
+
+module = {
+    name: "BlockAnimation",
+    author: "CzechHek",
+    version: 1.3,
+    values: list,
+    onMotion: function () {
+        mc.thePlayer.itemInUseCount = fake.get() && !KillAura.getValue("AutoBlock").get() && KillAuraClass.target && !!(heldItem = mc.thePlayer.getHeldItem()) && heldItem.getItem() instanceof Java.type("net.minecraft.item.ItemSword") ? 1337 : mc.thePlayer.itemInUseCount >= 1337 ? 0 : mc.thePlayer.itemInUseCount;
+        (KillAuraClass.blockingStatus || mc.thePlayer.itemInUseCount >= 1337) && (mc.thePlayer.swingProgress = [0.52, 0.58, 0.64, 0.7, 0.76, 0.82, 0.88, 0.94][animation.get()], mc.thePlayer.prevSwingProgress = mc.thePlayer.swingProgress - (0.01 * vibration.get()));
+    },
+    onDisable: function () {
+        mc.thePlayer.itemInUseCount = mc.thePlayer.itemInUseCount >= 1337 ? 0 : mc.thePlayer.itemInUseCount;
+    },
+    onLoad: function () {
+        moduleManager.getModule(this.name).state = true;
+    }
+}
+
+KillAuraClass = Java.type("net.ccbluex.liquidbounce.LiquidBounce").moduleManager.getModule(Java.type("net.ccbluex.liquidbounce.features.module.modules.combat.KillAura").class);
+KillAura = moduleManager.getModule("KillAura");
+
+script.import("Core.lib");
