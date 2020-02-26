7e879d05ea3bc83f4f4917e1892047eaea68b579
CzechHek <42714212+CzechHek@users.noreply.github.com>
2020-02-26 17:07:51 +0100

    Auto enable

--- Scripts/BlockAnimation.js
+++ Scripts/BlockAnimation.js
@@ -3,9 +3,13 @@ animation = value.createInteger("Animation", 3, 0, 7);
 module = {
     name: "BlockAnimation",
     author: "CzechHek",
+    version: 1.1,
     values: [animation],
     onMotion: function () {
         LiquidBounce.getModule(KillAura).blockingStatus && (mc.thePlayer.swingProgress = [0.52, 0.58, 0.64, 0.7, 0.76, 0.82, 0.88, 0.94][animation.get()]);
+    },
+    onLoad: function () {
+        moduleManager.getModule(this.name).state = true;
     }
 }
 
