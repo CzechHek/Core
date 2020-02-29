fdc8667756ce221680f5db35ae63958ef9f14b15
CzechHek <42714212+CzechHek@users.noreply.github.com>
2020-02-29 09:54:22 +0100

    Fixed bow issue

--- Scripts/BlockAnimation.js
+++ Scripts/BlockAnimation.js
@@ -7,14 +7,14 @@ list = [
 module = {
     name: "BlockAnimation",
     author: "CzechHek",
-    version: 1.4,
+    version: 1.5,
     values: list,
     onMotion: function () {
-        mc.thePlayer.itemInUseCount = (fake.get() && !KillAura.getValue("AutoBlock").get() && KillAuraClass.target && !!(heldItem = mc.thePlayer.getHeldItem()) && heldItem.getItem() instanceof Java.type("net.minecraft.item.ItemSword")) || mc.thePlayer.isBlocking() ? 1337 : mc.thePlayer.itemInUseCount >= 1337 ? 0 : mc.thePlayer.itemInUseCount;
-        (KillAuraClass.blockingStatus || mc.thePlayer.itemInUseCount >= 1337) && (mc.thePlayer.swingProgress = [0.52, 0.58, 0.64, 0.7, 0.76, 0.82, 0.88, 0.94][animation.get()], mc.thePlayer.prevSwingProgress = mc.thePlayer.swingProgress - (0.01 * vibration.get()));
+        mc.thePlayer.itemInUseCount = (fake.get() && !KillAura.getValue("AutoBlock").get() && KillAuraClass.target && !!(heldItem = mc.thePlayer.getHeldItem()) && heldItem.getItem() instanceof Java.type("net.minecraft.item.ItemSword")) || mc.thePlayer.isBlocking() ? 1337 : mc.thePlayer.itemInUseCount == 1337 ? 0 : mc.thePlayer.itemInUseCount;
+        (KillAuraClass.blockingStatus || mc.thePlayer.itemInUseCount == 1337) && (mc.thePlayer.swingProgress = [0.52, 0.58, 0.64, 0.7, 0.76, 0.82, 0.88, 0.94][animation.get()], mc.thePlayer.prevSwingProgress = mc.thePlayer.swingProgress - (0.01 * vibration.get()));
     },
     onDisable: function () {
-        mc.thePlayer.itemInUseCount = mc.thePlayer.itemInUseCount >= 1337 ? 0 : mc.thePlayer.itemInUseCount;
+        mc.thePlayer.itemInUseCount = mc.thePlayer.itemInUseCount == 1337 ? 0 : mc.thePlayer.itemInUseCount;
     },
     onLoad: function () {
         moduleManager.getModule(this.name).state = true;
