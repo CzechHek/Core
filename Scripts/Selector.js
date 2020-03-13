35b0ab64eb12f38c6f1e55dba7e5bdab97ea8c9b
CzechHek <42714212+CzechHek@users.noreply.github.com>
2020-03-14 00:58:36 +0100

    Recoded BlockSelector by yorik100

---
+++ Scripts/Selector.js
@@ -0,0 +1,22 @@
+list = [
+    mode = value.createList("Mode", ["Bed", "Dragon_Egg", "Obsidian", "Enchanting_Table", "Crafting_Table", "Custom"], "Bed"),
+    customid = value.createInteger("CustomID", 0, 0, 197),
+    fucker = value.createBoolean("Fucker", true),
+    blockesp = value.createBoolean("BlockESP", true)
+]
+
+module = {
+    name: "Selector",
+    author: "CzechHek, yorik100",
+    values: list,
+    onEnable: function () {
+        id = mode.get() == "Custom" ? customid.get() : [26, 122, 49, 116, 58][["Bed", "Dragon_Egg", "Obsidian", "Enchanting_Table", "Crafting_Table"].indexOf(mode.get())];
+        fucker.get() && moduleManager.getModule("Fucker").getValue("Block").set(id);
+        blockesp.get() && moduleManager.getModule("BlockESP").getValue("Block").set(id);
+    },
+    onUpdate: function () {
+        moduleManager.getModule(this.name).state = false;
+    }
+}
+
+script.import("Core.lib");
