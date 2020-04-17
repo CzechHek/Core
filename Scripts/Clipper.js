f1ee0747fcf1e6163d46dd31b8eb2e227b71ac71
CzechHek <42714212+CzechHek@users.noreply.github.com>
2020-04-17 22:38:46 +0200

    Update Clipper.js

--- Scripts/Clipper.js
+++ Scripts/Clipper.js
@@ -9,12 +9,16 @@ module = {
     name: "Clipper",
     author: "CzechHek",
     description: "VClips through blocks using space and shift.",
-    version: 2.0,
+    version: 2.1,
     values: list,
     onUpdate: function () {
         if (mc.thePlayer.onGround) {
-            for (i = 2, down = 0; i <= clipdown.get() + 2; i += 0.125) if (mc.theWorld.getCollidingBoundingBoxes(mc.thePlayer, mc.thePlayer.getEntityBoundingBox().offset(0,-i,0)).isEmpty() && !mc.theWorld.getCollidingBoundingBoxes(mc.thePlayer, mc.thePlayer.getEntityBoundingBox().offset(0,-i-0.125,0)).isEmpty()) down = i;
-            for (i = 2, up = 0; i <= clipup.get() + 2; i += 0.125) if (mc.theWorld.getCollidingBoundingBoxes(mc.thePlayer, mc.thePlayer.getEntityBoundingBox().offset(0,i,0)).isEmpty() && !mc.theWorld.getCollidingBoundingBoxes(mc.thePlayer, mc.thePlayer.getEntityBoundingBox().offset(0,i-0.125,0)).isEmpty()) up = i;
+            for (i = 2, down = 0; i <= clipdown.get() + 2; i += 0.125) if (mc.theWorld.getCollidingBoundingBoxes(mc.thePlayer, mc.thePlayer.getEntityBoundingBox().offset(0,-i,0)).isEmpty() && !mc.theWorld.getCollidingBoundingBoxes(mc.thePlayer, mc.thePlayer.getEntityBoundingBox().offset(0,-i-0.125,0)).isEmpty()) {
+                down = i; break;
+            }
+            for (i = 2, up = 0; i <= clipup.get() + 2; i += 0.125) if (mc.theWorld.getCollidingBoundingBoxes(mc.thePlayer, mc.thePlayer.getEntityBoundingBox().offset(0,i,0)).isEmpty() && !mc.theWorld.getCollidingBoundingBoxes(mc.thePlayer, mc.thePlayer.getEntityBoundingBox().offset(0,i-0.125,0)).isEmpty()) {
+                up = i; break;
+            }
             jumpStop = jumpStop && mc.gameSettings.keyBindJump.pressed;
             mc.gameSettings.keyBindJump.pressed = !jumpStop && mc.gameSettings.keyBindJump.pressed;
         }
