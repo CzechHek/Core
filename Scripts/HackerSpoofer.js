be5ebf5330d1c8b5af4a1ecba4ea2191ed01479d
CzechHek <42714212+CzechHek@users.noreply.github.com>
2020-06-29 13:48:41 +0200

    Specific player can now look at other players

--- Scripts/HackerSpoofer.js
+++ Scripts/HackerSpoofer.js
@@ -3,16 +3,18 @@ list = [
     look = value.createBoolean("Look", true),
     swing = value.createBoolean("Swing", true),
     specificplayer = value.createBoolean("SpecificPlayer", false),
-    nick = value.createText("Nick", "")
+    nick = value.createText("Nick", ""),
+    lookatnearest = value.createBoolean("LookAtNearest", true)
 ]
 
 module = {
     name: "HackerSpoofer",
     author: "CzechHek",
+    version: 1.1,
     values: list,
     onUpdate: function () {
-        if ((target = specificplayer.get() ? getPlayer(nick.get()) : getNearestTarget()) && target.getDistanceToEntity(mc.thePlayer) <= range.get()) {
-            diffX = target.posX - mc.thePlayer.posX; diffY = target.posY - mc.thePlayer.posY; diffZ = target.posZ - mc.thePlayer.posZ;
+        if ((target = specificplayer.get() ? getPlayer(nick.get()) : getNearestTarget(EntityPlayer)) && target.getDistanceToEntity(lookAt = (lookatnearest.get() && specificplayer.get() ? getNearestTarget(EntityPlayer, target) : mc.thePlayer)) <= range.get()) {
+            diffX = target.posX - lookAt.posX; diffY = target.posY - lookAt.posY; diffZ = target.posZ - lookAt.posZ;
             look.get() && (target.rotationYaw = target.rotationYawHead = ((Math.atan2(diffZ, diffX) * 180 / Math.PI) + 90), target.rotationPitch = Math.atan2(diffY, Math.sqrt(diffX * diffX + diffZ * diffZ)) * 180 / Math.PI);
             swing.get() && target.swingItem();
         }
