25a3eb61da1bc1fce3b57634a01dd41c72ebc66d
CzechHek <42714212+CzechHek@users.noreply.github.com>
2020-07-02 15:23:46 +0200

    Code improvements

--- Scripts/TowerScaffoldz.js
+++ Scripts/TowerScaffoldz.js
@@ -2,26 +2,19 @@ module = {
     name: "TowerScaffoldz",
     description: "Scaffold Addon",
     author: "CzechHek, yorik100",
-    version: 2.0,
+    version: 2.1,
     onEnable: function () {
-        scaffold.state = true; tower.state = false;
+        TowerModule.state = !(ScaffoldModule.state = true);
     },
     onDisable: function () {
-        scaffold.state = tower.state = false;
+        ScaffoldModule.state = TowerModule.state = false;
     },
-    onJump: function (event) {
-        !isMovingHorizontally() && event.cancelEvent();
+    onJump: function (e) {
+        !isMovingHorizontally() && e.cancelEvent();
     },
-    onMove: function (event) {
-        if (isMovingVertically() && !isMovingHorizontally()) {
-            event.setX(0); event.setZ(0);
-            scaffold.state = false; tower.state = true;
-        } else {
-            scaffold.state = true; tower.state = false;
-        }
+    onMove: function (e) {
+        !(ScaffoldModule.state = !(TowerModule.state = isMovingVertically() && !isMovingHorizontally())) && e.zeroXZ();
     }
 }
-
-scaffold = moduleManager.getModule("Scaffold"); tower = moduleManager.getModule("Tower");
 
 script.import("Core.lib");
