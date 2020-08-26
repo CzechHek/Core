c0eda5b64ce3d5dc262ed1e47775e8d1b5bdc15b
CzechHek <42714212+CzechHek@users.noreply.github.com>
2020-08-26 14:41:36 +0200

    Fixed bugs

--- Scripts/MatrixAntiBot.js
+++ Scripts/MatrixAntiBot.js
@@ -2,12 +2,12 @@
 (script = registerScript({
     name: "MatrixAntiBot",
     authors: ["CzechHek"],
-    version: "2.2"
+    version: "2.3"
 })).import("Core.lib");
 
 module = {
     category: "Combat",
     onPacket: function (e) {
-        (p = e.getPacket()) instanceof S38PacketPlayerListItem && p.getAction() == "ADD_PLAYER" && (name = p.getEntries().get(0).getProfile().getName()).length > 2 && mc.getNetHandler().getPlayerInfo(name) && (print("§2§lRemoved a bot§a:", name), e.cancelEvent());
+        e.getPacket() instanceof S38PacketPlayerListItem && e.getPacket().getAction() == "ADD_PLAYER" && (name = e.getPacket().getEntries().get(0).getProfile().getName()).length > 2 && name != mc.thePlayer.getName() && mc.getNetHandler().getPlayerInfo(name) && (print("§2§lRemoved a bot§a:", name), e.cancelEvent());
     }
 }
