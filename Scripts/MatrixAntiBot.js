3a1e70a12f424b3faa67e9b05f4a7fc0c772962e
CzechHek <42714212+CzechHek@users.noreply.github.com>
2020-08-30 00:14:39 +0200

    Fixed a bug

--- Scripts/MatrixAntiBot.js
+++ Scripts/MatrixAntiBot.js
@@ -2,17 +2,18 @@
 (script = registerScript({
     name: "MatrixAntiBot",
     authors: ["CzechHek"],
-    version: "3.3"
+    version: "3.31"
 })).import("Core.lib");
 
 module = {
     category: "Combat",
     onPacket: function (e) {
-        if (e.getPacket() instanceof S38PacketPlayerListItem && e.getPacket().getAction() == "ADD_PLAYER") {
-            name = (info = e.getPacket().getEntries().get(0)).getProfile().getName(); ping = info.getPing();
-            if (!wasAdded) wasAdded = name == mc.thePlayer.getName();
-            else if (!mc.thePlayer.isSpectator() && !mc.thePlayer.capabilities.allowFlying && ping && info.getGameMode() != "NOT_SET") e.cancelEvent(), print("§2§lRemoved a bot§a:", name);
-        } else if (e.getPacket() instanceof S41PacketServerDifficulty) wasAdded = false;
+        if (mc.thePlayer)
+            if (e.getPacket() instanceof S38PacketPlayerListItem && e.getPacket().getAction() == "ADD_PLAYER") {
+                name = (info = e.getPacket().getEntries().get(0)).getProfile().getName(); ping = info.getPing();
+                if (!wasAdded) wasAdded = name == mc.thePlayer.getName();
+                else if (!mc.thePlayer.isSpectator() && !mc.thePlayer.capabilities.allowFlying && ping && info.getGameMode() != "NOT_SET") e.cancelEvent(), print("§2§lRemoved a bot§a:", name);
+            } else if (e.getPacket() instanceof S41PacketServerDifficulty) wasAdded = false;
     }
 }
 
