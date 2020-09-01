866332eee1394c84b06abc1cecf74111b96d85b6
CzechHek <42714212+CzechHek@users.noreply.github.com>
2020-09-01 16:06:11 +0200

    PacketMode, SimulatedTimer

--- Scripts/BlinkFall.js
+++ Scripts/BlinkFall.js
@@ -2,44 +2,56 @@
 (script = registerScript({
     name: "BlinkFall",
     authors: ["CzechHek"],
-    version: "3.4"
+    version: "4.1"
 })).import("Core.lib");
 
 module = {
     category: "Movement",
-    values: [maxfalltime = value.createInteger("MaxFallTime", 1500, 0, 10000), nofall = value.createBoolean("NoFall", true), maxpackets = value.createInteger("MaxPackets", 100, 1, 100)],
+    values: [
+        maxfalltime = value.createInteger("MaxFallTime", 1500, 0, 10000),
+        nofall = value.createBoolean("NoFall", true),
+        packetmode = value.createList("PacketMode", ["SimulatedDelay", "Instant"], "Instant"), simulatedtimer = value.createFloat("SimulatedTimer", 1, 1, 10)
+    ],
     onPacket: function (e) {
         if (catchPackets && e.getPacket() instanceof C03PacketPlayer) {
             if (!sendPackets) {
                 e.cancelEvent(); packet = e.getPacket();
                 packet.onGround ? (sendPackets = mc.thePlayer.posY != lastPos[1]) : (nofall.get() && (packet.onGround = mc.thePlayer.fallDistance > 3) && (mc.thePlayer.fallDistance = 0));
-                packets.push(packet);
-            } else !packets.includes(e.getPacket()) && e.cancelEvent();
+                !packets.length && (time = System.currentTimeMillis());
+                packets.push([packet, packetmode.get() == "SimulatedDelay" ? (System.currentTimeMillis() - time) / simulatedtimer.get() : 0]);
+            } else e.getPacket() != lastPacket && e.cancelEvent();
         }
     },
     onMove: function (e) {
         if (!catchPackets) {
-            if (!ScaffoldModule.state && !TowerModule.state) {
+            if (!ScaffoldModule.state && !TowerModule.state && !mc.thePlayer.isSneaking() && !mc.thePlayer.isOnLadder() && !mc.thePlayer.isInWater()) {
                 yaw = MovementUtils.getDirection();
                 mc.thePlayer.onGround && (lastPos = [mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ]);
-                if (isMovingHorizontally() && mc.theWorld.getCollidingBoundingBoxes(mc.thePlayer, mc.thePlayer.getEntityBoundingBox().offset(-Math.sin(yaw) * e.getX(), -2, Math.cos(yaw) * e.getZ()).expand(0, 2, 0)).isEmpty())
-                    catchPackets = !timer.reset();
+                if (isMovingHorizontally() && !isSafe(e))
+                    catchPackets = !timer.reset(), print("catching");
             }
         } else {
             if (timer.hasTimePassed(maxfalltime.get()) || sendPackets || mc.thePlayer.isOnLadder() || mc.thePlayer.isInWater()) {
-                if (sendPackets || mc.thePlayer.isOnLadder() || mc.thePlayer.isInWater()) {
-                    sendPackets = true;
-                    for (i = -1; (i++ < maxpackets.get()) && (i < packets.length);) mc.getNetHandler().addToSendQueue(packets[i]);
-                    packets.splice(0, maxpackets.get());
-                    packets.length ? (e.zeroXZ(), e.setY(-0.078)) : (sendPackets = false);
-                } else
-                    mc.thePlayer.setPositionAndUpdate(lastPos[0], lastPos[1], lastPos[2]),
-                    e.zeroXZ(), mc.thePlayer.motionX = mc.thePlayer.motionZ = 0,
-                    packets = [];
-                catchPackets = packets.length;
+                if (sendPackets || isSending || mc.thePlayer.isOnLadder() || mc.thePlayer.isInWater()) {
+                    if (!isSending) {
+                        isSending = true;
+                        packets.forEach(function (p, i) {timeout(p[1], function () {mc.getNetHandler().addToSendQueue(lastPacket = p[0]); i == packets.length - 1 && (packets = [])})});
+                    }
+                    packets.length ? (e.zeroXZ(), e.setY(-0.078)) : (sendPackets = isSending = false);
+                } else {
+                    mc.thePlayer.setPositionAndUpdate(lastPos[0], lastPos[1], lastPos[2]);
+                    e.zeroXZ(); mc.thePlayer.motionX = mc.thePlayer.motionZ = 0;
+                    packets = []; print("antivoid");
+                }
+                !(catchPackets = packets.length) && print("end");
             }
         }
     }
 }
 
-var packets = [], timer = new MSTimer(), catchPackets, lastPos = [], sendPackets = false;
+function isSafe(e) {
+    blockPos = new BlockPos(mc.thePlayer.posX + e.getX(), mc.thePlayer.posY + 2, mc.thePlayer.posZ + e.getZ());
+    for (i = -1; i++ < 6;) if (!mc.theWorld.isAirBlock(blockPos.down(i))) return true
+}
+
+var packets = [], timer = new MSTimer(), catchPackets, lastPos = [], sendPackets = false, System = Java.type("java.lang.System"), time, isSending, lastPacket;
