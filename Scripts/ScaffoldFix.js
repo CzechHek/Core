4b9197758836e36fa0f032cb768c69a2f11bc47a
CzechHek <42714212+CzechHek@users.noreply.github.com>
2020-10-10 21:11:16 +0200

    Removed old forgotten stuff

--- Scripts/ScaffoldFix.js
+++ Scripts/ScaffoldFix.js
@@ -2,14 +2,14 @@
 (script = registerScript({
     name: "ScaffoldFix",
     authors: ["CzechHek"],
-    version: "1.1"
+    version: "1.2"
 })).import("Core.lib");
 
 module = {
     category: "World",
     description: "Fixes Scaffold AutoBlock detections.",
     onPacket: function (e) {
-        (p = e.getPacket()) instanceof C09PacketHeldItemChange && (e.getPacket().getSlotId() == prevSlot ? e.cancelEvent() : (prevSlot = e.getPacket().getSlotId()));
+        e.getPacket() instanceof C09PacketHeldItemChange && (e.getPacket().getSlotId() == prevSlot ? e.cancelEvent() : (prevSlot = e.getPacket().getSlotId()));
     }
 }
 
