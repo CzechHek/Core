e4ea9a024de73c251ac82004f09ce9acd880be18
CzechHek <42714212+CzechHek@users.noreply.github.com>
2020-10-12 14:09:32 +0200

    Fixed a bug when using eagle, now hides modules from array

--- Scripts/TowerScaffoldz.js
+++ Scripts/TowerScaffoldz.js
@@ -1,7 +1,7 @@
 ///api_version=2
 (script = registerScript({
     name: "TowerScaffoldz",
-    version: "1.7",
+    version: "1.8",
     authors: ["CzechHek", "yorik100"]
 })).import("Core.lib");
 
@@ -9,15 +9,17 @@ module = {
     category: "World",
     description: "Scaffold Addon",
     onEnable: function () {
-        TowerModule.state = !(ScaffoldModule.state = true);
+        hideScaffold = ScaffoldModule.array; hideTower = TowerModule.array;
+        ScaffoldModule.array = TowerModule.array = TowerModule.state = !(ScaffoldModule.state = true);
     },
     onDisable: function () {
         ScaffoldModule.state = TowerModule.state = false;
+        ScaffoldModule.array = hideScaffold; TowerModule.array = hideTower;
     },
     onJump: function (e) {
-        !isMovingHorizontally() && e.cancelEvent();
+        !isInputHorizontally() && e.cancelEvent();
     },
     onMove: function (e) {
-        !(ScaffoldModule.state = !(TowerModule.state = isMovingVertically() && !isMovingHorizontally())) && e.zeroXZ();
+        !(ScaffoldModule.state = !(TowerModule.state = isInputVertically() && !isInputHorizontally())) && e.zeroXZ();
     }
 }
