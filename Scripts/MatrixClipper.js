3f6408ad584e9d3fbaf484f385609179abdd3e4d
CzechHek <42714212+CzechHek@users.noreply.github.com>
2021-03-10 20:55:07 +0100

    They said that .vclip -3 still worked

--- Scripts/MatrixClipper.js
+++ Scripts/MatrixClipper.js
@@ -2,7 +2,7 @@
 (script = registerScript({
     name: "MatrixClipper",
     authors: ["CzechHek"],
-    version: "1.1"
+    version: "1.2"
 })).import("Core.lib");
 
 module = {
@@ -10,11 +10,6 @@ module = {
     description: "Automatically clips you from cage.",
     values: maxticksexisted = value.createInteger("MaxTicksExisted", 10, 1, 100),
     onUpdate: function () {
-        if (mc.thePlayer.ticksExisted <= maxticksexisted.get() && mc.theWorld.getBlockState(new BlockPos(mc.thePlayer).up(3)).getBlock() == Blocks.glass) {
-            for (i = 3; i < 12; i++) 
-                if (!mc.theWorld.getCollidingBoundingBoxes(mc.thePlayer, mc.thePlayer.getEntityBoundingBox().offset(0, -i, 0)).isEmpty())
-                    return mc.thePlayer.setPositionAndUpdate(mc.thePlayer.posX, mc.thePlayer.posY - i + 1, mc.thePlayer.posZ);
-            mc.thePlayer.setPositionAndUpdate(mc.thePlayer.posX, mc.thePlayer.posY - 10, mc.thePlayer.posZ);
-        }
+        if (mc.thePlayer.ticksExisted <= maxticksexisted.get() && mc.theWorld.getBlockState(new BlockPos(mc.thePlayer).up(3)).getBlock() == Blocks.glass) mc.thePlayer.setPositionAndUpdate(mc.thePlayer.posX, mc.thePlayer.posY - 3, mc.thePlayer.posZ);
     }
 }
