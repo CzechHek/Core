0760b9781b60aae18e91b8ede01652320496b7b8
CzechHek <czechhek@gmail.com>
2021-10-29 16:01:09 +0200

    Added SprintPatch

---
+++ Scripts/SprintPatch.js
@@ -0,0 +1,31 @@
+///api_version=2
+(script = registerScript({
+    name: "SprintPatch",
+    authors: ["CzechHek", "yorik100"],
+    version: "1.0"
+})).import("Core.lib");
+
+module = {
+    description: "Bypasses alldirectional sprinting on various anticheats and fixes jumping direction.",
+    category: "Patches",
+    values: [
+        nopacketspatch = value.createBoolean("NoPacketsPatch", true),
+        jumpdirectionpatch = value.createBoolean("JumpDirectionPatch", true)
+    ],
+    onPacket: function (e) {
+        if (nopacketspatch.get() && e.getPacket() instanceof C0BPacketEntityAction && (e.getPacket().getAction() == "START_SPRINTING" || e.getPacket().getAction() == "STOP_SPRINTING")) e.cancelEvent();
+    },
+    onJump: function (e) {
+        if (jumpdirectionpatch.get() && !modified) {
+            e.cancelEvent();
+            var prevYaw = mc.thePlayer.rotationYaw;
+            mc.thePlayer.rotationYaw = Math.toDegrees(MovementUtils.getDirection());
+            modified = true;
+            mc.thePlayer.jump();
+            mc.thePlayer.rotationYaw = prevYaw;
+            modified = false;
+        }
+    }
+}
+
+var modified;
