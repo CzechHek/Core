29ad56ba8975e467084e653df99700f682d0cf0f
CzechHek <czechhek@gmail.com>
2021-12-23 22:52:37 +0100

    Added timer option

--- Scripts/MatrixStep.js
+++ Scripts/MatrixStep.js
@@ -1,31 +1,36 @@
 ///api_version=2
 (script = registerScript({
     name: "MatrixStep",
-    version: "2.0",
+    version: "2.1",
     authors: ["CzechHek", "Neogardo"]
 })).import("Core.lib");
 
 module = {
     description: "Step bypassing Matrix.",
     category: "Movement",
-    values: predict = value.createFloat("Predict", 0.45, 0.0, 1.0),
+    values: [
+        predict = value.createFloat("Predict", 0.45, 0.0, 1.0),
+        timer = value.createFloat("Timer", 1, 0.1, 3)
+    ],
     onPacket: function (e) {
         if (isStepping) {
             var packet = e.getPacket();
-            if (packet instanceof C03PacketPlayer) {
-                packet.onGround = true;
-            }
+            if (packet instanceof C03PacketPlayer) packet.onGround = true;
         }
     },
     onUpdate: function () {
-        isStepping = canStep(1, predict.get()) && isMovingHorizontally() && !mc.thePlayer.fallDistance;
+        if (canStep(1, predict.get()) && isMovingHorizontally() && !mc.thePlayer.fallDistance) isStepping = mc.timer.timerSpeed = timer.get();
+        else {
+            mc.timer.timerSpeed = 1;
+            isStepping = false;
+        }
         if (isStepping) {
             mc.thePlayer.onGround = true;
             mc.thePlayer.motionY = 0;
             mc.thePlayer.jump();
             isStepping = false
-        }
+        } 
     }
 }
 
-var isStepping;
+var isStepping = false;
