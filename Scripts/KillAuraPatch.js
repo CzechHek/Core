b66e1e7f475b09eaa3965b792f6f06f60cab3857
CzechHek <czechhek@gmail.com>
2022-04-13 23:32:14 +0200

    KillAuraPatch 3 - autoblock modes, criticals
    Added AutoBlockPatch modes:
    - Packet, AfterTick (supports LB legacy)
    - new mode LowHurtTime
    
    LowHurtTime AutoBlock mode is used with low KillAura HurtTime (max 3). It is possible to bypass any strafe, movement and AutoBlock checks because you only hit your opponent when he can be hurt. You aim, attack, ... accordingly and otherwise block all the time (LB KillAura doesn't block inbetween hits with low HurtTime).
    
    Added OwnHurtTimeCheck:
    Stops you from blocking if you cannot be hurt because you already got hit earlier.
    
    Added HurtTimeCriticals:
    They check if your target has a specific hurt time and jump accordingly. It can either jump legitimately or silently use the default Speed module to do so. By doing this you can legitimately hit almost all critical hits and therefore bypass any Criticals check.
    
    In conclusion, with KillAuraPatch 3 you can win HvHs on Matrix and other anticheats with like 7 hearts left.

--- Scripts/KillAuraPatch.js
+++ Scripts/KillAuraPatch.js
@@ -2,30 +2,48 @@
 (script = registerScript({
     name: "KillAuraPatch",
     authors: ["CzechHek"],
-    version: "2.0"
+    version: "3.0"
 })).import("Core.lib");
 
 list = [
-    rotationspatch = new (Java.extend(BoolValue)) ("RotationsPatch", true) {onChanged: function () updateValues()},
-    gcdsensitivity = value.createInteger("GCDSensitivity", mc.gameSettings.mouseSensitivity / 0.005, 0, 200),
-
-    autoblockpatch = new (Java.extend(BoolValue)) ("AutoBlockPatch", true) {onChanged: function () updateValues()},
-    maxrange = value.createFloat("MaxRange", 3.5, 3, 8),
-    itemcheck = value.createBoolean("ItemCheck", true),
-
-    facingcheck = new (Java.extend(BoolValue)) ("FacingCheck", true) {onChanged: function () updateValues()},
-    toleration = value.createFloat("Toleration", 0, 0, 2),
-
-    dynamicrange = new (Java.extend(BoolValue)) ("DynamicRange", true) {onChanged: function () updateValues()},
-    changeon = value.createList("ChangeOn", ["Attack", "Update"], "Attack"),
-    minstillrange = new (Java.extend(FloatValue)) ("MinStillRange", 3, 3, 8) {onChanged: function (o, n) n > maxstillrange.get() && minstillrange.set(maxstillrange.get())},
-    maxstillrange = new (Java.extend(FloatValue)) ("MaxStillRange", 3.2, 3, 8) {onChanged: function (o, n) n < minstillrange.get() && maxstillrange.set(minstillrange.get())},
-    minwalkingrange = new (Java.extend(FloatValue)) ("MinWalkingRange", 3.4, 3, 8) {onChanged: function (o, n) n > maxwalkingrange.get() && minwalkingrange.set(maxwalkingrange.get())},
-    maxwalkingrange = new (Java.extend(FloatValue)) ("MaxWalkingRange", 3.6, 3, 8) {onChanged: function (o, n) n < minwalkingrange.get() && maxwalkingrange.set(minwalkingrange.get())},
-    minrunningrange = new (Java.extend(FloatValue)) ("MinRunningRange", 3.7, 3, 8) {onChanged: function (o, n) n > maxrunningrange.get() && minrunningrange.set(maxrunningrange.get())},
-    maxrunningrange = new (Java.extend(FloatValue)) ("MaxRunningRange", 3.9, 3, 8) {onChanged: function (o, n) n < minrunningrange.get() && maxrunningrange.set(minrunningrange.get())},
-    minjumpingrange = new (Java.extend(FloatValue)) ("MinJumpingRange", 3.7, 3, 8) {onChanged: function (o, n) n > maxjumpingrange.get() && minjumpingrange.set(maxjumpingrange.get())},
-    maxjumpingrange = new (Java.extend(FloatValue)) ("MaxJumpingRange", 3.9, 3, 8) {onChanged: function (o, n) n < minjumpingrange.get() && maxjumpingrange.set(minjumpingrange.get())}
+    rotationValues = [
+        rotationspatch = new (Java.extend(BoolValue)) ("RotationsPatch", false) { onChanged: function () updateValues() },
+        gcdsensitivity = value.createInteger("GCDSensitivity", mc.gameSettings.mouseSensitivity / 0.005, 0, 200)
+    ],
+    autoBlockValues1 = [
+        autoblockpatch = new (Java.extend(BoolValue)) ("AutoBlockPatch", true) { onChanged: function () updateValues() },
+        blockmode = new (Java.extend(ListValue)) ("BlockMode", ["Packet", "AfterTick", "LowHurtTime"], "LowHurtTime") { onChanged: function () updateValues() }
+    ],
+    reblockdelay = value.createInteger("ReblockDelay", 2, 0, 5),
+    autoBlockValues2 = [
+        blockrange = value.createFloat("BlockRange", 4.5, 3, 8),
+        weaponcheck = value.createBoolean("WeaponCheck", true),
+    ],
+    autoBlockValues3 = [
+        ownhurttimecheck = new (Java.extend(BoolValue)) ("OwnHurtTimeCheck", false) { onChanged: function () updateValues() },
+        maxownhurttime = value.createInteger("MaxOwnHurtTime", 3, 0, 10)
+    ],
+    autoBlockValues4 = [
+        facingcheck = new (Java.extend(BoolValue)) ("FacingCheck", true) { onChanged: function () updateValues() },
+        toleration = value.createFloat("OffsetToleration", 0.5, 0, 2)
+    ],
+    criticalsValues = [
+        hurttimecriticals = new (Java.extend(BoolValue)) ("HurtTimeCriticals", true) { onChanged: function () updateValues() },
+        maxhurttimetojump = value.createInteger("MaxHurtTimeToJump", 4, 0, 10),
+        jumpmode = value.createList("JumpMode", ["Legit", "SpeedModule"], "Legit")
+    ],
+    dynamicRangeValues = [
+        dynamicrange = new (Java.extend(BoolValue)) ("DynamicRange", false) { onChanged: function () updateValues() },
+        changeon = value.createList("ChangeOn", ["Attack", "Update"], "Attack"),
+        minstillrange = new (Java.extend(FloatValue)) ("MinStillRange", 3, 3, 8) { onChanged: function (o, n) n > maxstillrange.get() && minstillrange.set(maxstillrange.get()) },
+        maxstillrange = new (Java.extend(FloatValue)) ("MaxStillRange", 3.2, 3, 8) { onChanged: function (o, n) n < minstillrange.get() && maxstillrange.set(minstillrange.get()) },
+        minwalkingrange = new (Java.extend(FloatValue)) ("MinWalkingRange", 3.4, 3, 8) { onChanged: function (o, n) n > maxwalkingrange.get() && minwalkingrange.set(maxwalkingrange.get()) },
+        maxwalkingrange = new (Java.extend(FloatValue)) ("MaxWalkingRange", 3.6, 3, 8) { onChanged: function (o, n) n < minwalkingrange.get() && maxwalkingrange.set(minwalkingrange.get()) },
+        minrunningrange = new (Java.extend(FloatValue)) ("MinRunningRange", 3.7, 3, 8) { onChanged: function (o, n) n > maxrunningrange.get() && minrunningrange.set(maxrunningrange.get()) },
+        maxrunningrange = new (Java.extend(FloatValue)) ("MaxRunningRange", 3.9, 3, 8) { onChanged: function (o, n) n < minrunningrange.get() && maxrunningrange.set(minrunningrange.get()) },
+        minjumpingrange = new (Java.extend(FloatValue)) ("MinJumpingRange", 3.7, 3, 8) { onChanged: function (o, n) n > maxjumpingrange.get() && minjumpingrange.set(maxjumpingrange.get()) },
+        maxjumpingrange = new (Java.extend(FloatValue)) ("MaxJumpingRange", 3.9, 3, 8) { onChanged: function (o, n) n < minjumpingrange.get() && maxjumpingrange.set(minjumpingrange.get()) }
+    ]
 ]
 
 module = {
@@ -41,46 +59,145 @@ module = {
             p.pitch -= p.pitch % gcd;
             p.yaw -= p.yaw % gcd;
         }
+        if (e.getPacket() instanceof C02PacketUseEntity) unblock();
+        else if (e.getPacket() instanceof C07PacketPlayerDigging && e.getPacket().getStatus() == "RELEASE_USE_ITEM") wasBlocking = false;
+
     },
     onUpdate: function () {
-        if (autoblockpatch.get() && (target = KillAuraModule.target)) {
-            shouldBlock = false;
-            if (!itemcheck.get() || (target.getHeldItem() && (item = target.getHeldItem().getItem()) && (item instanceof ItemSword || item instanceof ItemAxe))) {
-                if (PlayerExtensionKt.getDistanceToEntityBox(target, mc.thePlayer) < maxrange.get()) {
-                    if (facingcheck.get()) {
-                        if (target.rayTrace(maxrange.get(), 1).typeOfHit == "MISS") {
-                            eyesVec = target.getPositionEyes(1);
-                            lookVec = target.getLook(1);
-                            pointingVec = eyesVec.addVector(lookVec.xCoord * maxrange.get(), lookVec.yCoord * maxrange.get(), lookVec.zCoord * maxrange.get())
-                            border = mc.thePlayer.getCollisionBorderSize() + toleration.get();
-                            bb = mc.thePlayer.getEntityBoundingBox().expand(border, border, border);
-                            shouldBlock = !!bb.calculateIntercept(eyesVec, pointingVec) || bb.intersectsWith(target.getEntityBoundingBox());
+        reblockTimer.update();
+        if (KillAuraModule.state) {
+            autoblockpatch.get() && autoBlockValue.set(blockmode.get() == "LowHurtTime" ? "Off" : blockmode.get());
+            dynamicrange.get() && changeon.get() == "Update" && updateRange();
+
+            if (autoblockpatch.get() && mc.thePlayer.getHeldItem() && mc.thePlayer.getHeldItem().getItem() instanceof ItemSword) {
+                var shouldBlock = false;
+                if (!ownhurttimecheck.get() || mc.thePlayer.hurtTime <= maxownhurttime.get()) {
+                    for each (var target in getTargetsInRange(blockrange.get())) {
+                        if (!weaponcheck.get() || (target.getHeldItem() && (item = target.getHeldItem().getItem()) && (item instanceof ItemSword || item instanceof ItemAxe))) {
+                            if (facingcheck.get()) {
+                                if (target.rayTrace(blockrange.get(), 1).typeOfHit == "MISS") {
+                                    eyesVec = target.getPositionEyes(1);
+                                    lookVec = target.getLook(1);
+                                    pointingVec = eyesVec.addVector(lookVec.xCoord * blockrange.get(), lookVec.yCoord * blockrange.get(), lookVec.zCoord * blockrange.get())
+                                    border = mc.thePlayer.getCollisionBorderSize() + toleration.get();
+                                    bb = mc.thePlayer.getEntityBoundingBox().expand(border, border, border);
+                                    shouldBlock = !!bb.calculateIntercept(eyesVec, pointingVec) || bb.intersectsWith(target.getEntityBoundingBox());
+                                }
+                            } else shouldBlock = true;
                         }
-                    } else shouldBlock = true;
+                        
+                        if (shouldBlock) break
+                    }
+                }
+
+                autoBlockValue.set(!shouldBlock || blockmode.get() == "LowHurtTime" ? "Off" : blockmode.get());
+                if (blockmode.get() == "LowHurtTime") {
+                    if (shouldBlock) {
+                        if (!wasBlocking && reblockTimer.hasTimePassed(reblockdelay.get() + 1)) {
+                            sendPacket(new C08PacketPlayerBlockPlacement(new BlockPos(-1, -1, -1), 255, mc.thePlayer.getHeldItem(), 0, 0, 0), true)
+                            reblockTimer.reset();
+                            wasBlocking = true;
+                            mc.thePlayer.itemInUseCount = 1;
+                        }
+                    } else unblock();
                 }
             }
-            VALUE_FIELD.set(autoBlockValue, shouldBlock);
+
+            if (hurttimecriticals.get() && mc.thePlayer.onGround) {
+                var targets = getTargetsInRange(rangeValue.get()), shouldJump = targets.some(function (e) e.hurtTime <= maxhurttimetojump.get());
+                if (jumpmode.get() == "Legit") shouldJump && mc.thePlayer.jump();
+                else if (targets.length) {
+                    if (prevSpeed === null) prevSpeed = SpeedModule.state;
+                    ARRAY_FIELD.set(SpeedModule, false);
+                    STATE_FIELD.set(SpeedModule, shouldJump);
+                    if (shouldJump) mc.thePlayer.movementInput.moveForward = 1;
+                    return
+                }
+            }
         }
 
-        dynamicrange.get() && changeon.get() == "Update" && updateRange();
+        if (mc.thePlayer.onGround && prevSpeed !== null) {
+            STATE_FIELD.set(SpeedModule, prevSpeed);
+            ARRAY_FIELD.set(SpeedModule, true);
+            prevSpeed = null;
+        }
     },
     onAttack: function () dynamicrange.get() && changeon.get() == "Attack" && updateRange(),
     onLoad: function () {
         autoBlockValue = KillAuraModule.getValue("AutoBlock");
         rangeValue = KillAuraModule.getValue("Range");
+        hurtTimeValue = KillAuraModule.getValue("HurtTime");
     },
-    onEnable: function () prevValues = [autoBlockValue.get(), rangeValue.get()],
+    onEnable: function () {
+        prevHurtTime = hurtTimeValue.get();
+        prevAutoBlock = autoBlockValue.get();
+        prevRange = rangeValue.get();
+        updateValues();
+    },
     onDisable: function () {
-        autoBlockValue.set(prevValues[0]);
-        rangeValue.set(prevValues[1]);
+        revertHurtTime();
+        autoBlockValue.set(prevAutoBlock);
+        rangeValue.set(prevRange);
+        if (prevSpeed !== null) {
+            SpeedModule.state = prevSpeed;
+            prevSpeed = null;
+            ARRAY_FIELD.set(SpeedModule, true);
+        }
     },
     onClickGuiOpen: function () updateValues(),
     onClickGuiClosed: function () updateValues(true)
 }
 
+//To prevent config updates, sound effects, array animations
 VALUE_FIELD = getField(Value, "value");
-Float = Java.type("java.lang.Float");
+STATE_FIELD = getField(Module, "state");
+ARRAY_FIELD = getField(Module, "array");
+MAXIMUM_FIELD = getField(IntegerValue, "maximum");
 
-function updateValues(closing) setValues(KillAuraPatchModule, closing ? list : (rotationspatch.get() ? [rotationspatch, gcdsensitivity] : [rotationspatch]).concat(autoblockpatch.get() ? [autoblockpatch, maxrange, itemcheck].concat(facingcheck.get() ? [facingcheck, toleration] : facingcheck) : autoblockpatch).concat(dynamicrange.get() ? [dynamicrange, changeon, minstillrange, maxstillrange, minwalkingrange, maxwalkingrange, minrunningrange, maxrunningrange, minjumpingrange, maxjumpingrange] : dynamicrange));
+Float = Java.type("java.lang.Float"),
+reblockTimer = new TickTimer();
+
+var wasBlocking, prevHurtTime, prevSpeed = null;
+
+
+
+function revertHurtTime() {
+    if (hurtTimeValue.getMaximum() == 3) {
+        MAXIMUM_FIELD.set(hurtTimeValue, 10);
+        hurtTimeValue.set(prevHurtTime);
+    }
+}
+
+function updateValues(closing) {
+    if (!closing) {
+        if (autoblockpatch.get() && blockmode.get() == "LowHurtTime") {
+            if (hurtTimeValue.getMaximum() > 3) {
+                prevHurtTime = hurtTimeValue.get();
+                MAXIMUM_FIELD.set(hurtTimeValue, 3);
+                if (hurtTimeValue.get() > 3) hurtTimeValue.set(3);
+            }
+        } else if (hurtTimeValue.getMaximum() == 3) revertHurtTime();
+    }
+
+    setValues(KillAuraPatchModule, closing ? list : 
+        [].concat(rotationspatch.get() ? rotationValues : rotationspatch)
+        .concat(autoblockpatch.get() ? autoBlockValues1
+            .concat(blockmode.get() == "LowHurtTime" ? [hurtTimeValue, reblockdelay] : null)
+            .concat(autoBlockValues2)
+            .concat(ownhurttimecheck.get() ? autoBlockValues3 : ownhurttimecheck)
+            .concat(facingcheck.get() ? autoBlockValues4 : facingcheck)
+        : autoblockpatch)
+        .concat(hurttimecriticals.get() ? criticalsValues : hurttimecriticals)
+        .concat(dynamicrange.get() ? dynamicRangeValues : dynamicrange))
+} 
 
 function updateRange() VALUE_FIELD.set(rangeValue, new Float(rand(isInputVertically(true) ? [minjumpingrange.get(), maxjumpingrange.get()] : isMovingHorizontally() ? mc.thePlayer.isSprinting() ? [minrunningrange.get(), maxrunningrange.get()] : [minwalkingrange.get(), maxwalkingrange.get()] : [minstillrange.get(), maxstillrange.get()])));
+
+function unblock() {
+    if (wasBlocking && blockmode.get() == "LowHurtTime") {
+        sendPacket(new C07PacketPlayerDigging(C07PacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, EnumFacing.DOWN), true);
+        wasBlocking = false
+        mc.thePlayer.itemInUseCount = 0;
+        reblockTimer.reset();
+    }
+}
